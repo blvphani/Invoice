2adcb5c77c1cb9089e727a2895d870c7a4625ba8
manjulaputraphani <manjulaputra@gmail.com>
2025-11-16 19:49:18 +0530

    added models structure and layer connected

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using MediatR;
 using SriDurgaHariHaraBackend.Application;
 using SriDurgaHariHaraBackend.Data.Persistence;
+using SriDurgaHariHaraBackend.Api.Attributes;
+using SriDurgaHariHaraBackend.Data.Models;
+using SriDurgaHariHaraBackend.Application.ProductValidations;
+using FluentValidation;
+using SriDurgaHariHaraBackend.Application.Interfaces;
+using SriDurgaHariHaraBackend.Data.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,21 +24,39 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 // Register MediatR and discover handlers in the Application assembly
 
-builder.Services.AddMediatR(typeof(ProductDto).Assembly);
+builder.Services.AddMediatR(typeof(Invoice).Assembly);
+
+// Register FluentValidation validators from the Application assembly
+// Replace 'CreateProductValidator' with any validator type in your Application project
+builder.Services.AddValidatorsFromAssemblyContaining<InvoiceValidation>();
 
 // (optional) register your DI extension if present
-// builder.Services.AddSriDurgaServices(builder.Configuration);
+//builder.Services.AddSriDurgaServices(builder.Configuration);
+
+// Register application/data services as needed
+builder.Services.AddScoped<IInvoiceRepository, InvoiceReadRepo>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+    {
+        // ensure the swagger UI appears at /swagger/index.html
+        options.RoutePrefix = "swagger";
+        // or to host swagger at root use RoutePrefix = string.Empty;
+    });
+
+app.UseRouting();
+// use the middleware early
+app.UseMiddleware<RequestValidationMiddleware>();
+
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
